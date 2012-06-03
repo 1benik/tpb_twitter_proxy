ff3e9a2bb47d0239e9c3271eac119e845c938d4c
Jim-David Bennink <jd.bennink@gmail.com>
2012-06-03 14:43:33 +0200

    Various things
    - Reorganized layout.
    - Added "modified by" notice.
    - Added import "Set".
    - Fixed Python "Indexerror".
    - Added code to auto-follow followers.
    - Fixed Python "json" error.

--- proxy.py
+++ proxy.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
-###############################################################################
+##############################################################################################
 # Copyright (c) 2012, Floor Terra <floort@gmail.com>
-# 
+#
 # Permission to use, copy, modify, and/or distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
 # copyright notice and this permission notice appear in all copies.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
 # WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
 # MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
@@ -14,7 +14,9 @@
 # WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
 # ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 # OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-############################################################################### 
+###############################################################################################
+# MODIFIED BY JIM-DAVID BENNINK <www.thepiratebayproxynl.nl> <webmaster@thepiratebayproxynl.nl> 
+###############################################################################################
 
 import random
 import urlparse
@@ -22,8 +24,13 @@ import urllib
 import urllib2
 import twitter
 import time
+from sets import Set
 
-ACCOUNT_NAME = "@floorter" # Running as this user
+ACCOUNT_NAME = "@TBPProxyNL" # Running as this user
+
+
+
+
 CONSUMER_KEY = ""
 CONSUMER_SECRET = ""
 ACCESS_TOKEN_KEY = ""
@@ -47,10 +54,12 @@ def shorten_magnet(m):
             q += p[0] + "=" + p[1] + "&"
         else:
             keep.append(p)
-    keep.append(random.choice(keepone))
-    q += urllib.urlencode(keep)
-    return q
-
+	if not keepone:
+		print False
+	else:
+		keep.append(random.choice(keepone))
+		q += urllib.urlencode(keep)
+		return q
 
 def search(q):
     url = "http://thepiratebay.se/search/%s/0/7/0" % urllib.quote(q)
@@ -69,21 +78,39 @@ api = twitter.Api(
     access_token_secret=ACCESS_TOKEN_SECRET
 )
 
+following = api.GetFriends()
+
+friendNames = Set()
+for friend in following:
+	friendNames.add(friend.screen_name)
+	
+followers = api.GetFollowers()
+for follower in followers:
+	if (not follower.screen_name in friendNames):
+		try:
+			api.CreateFriendship(follower.screen_name)
+		except twitter.TwitterError:
+			print False
 
 while True:
     f = open("last.id")
     last = int(f.read())
     f.close()
-    mentions = api.GetMentions(since_id=last)
-    for m in mentions:
-        magnet = search(m.text.replace(ACCOUNT_NAME, ""))
-        update_str = "@%s %s" % (m.user.screen_name, shorten_magnet(magnet))
-        update = api.PostUpdate(update_str, in_reply_to_status_id=m.id)
-        f = open("last.id", "w")
-        f.write("%d\n" % (m.id,))
-        f.close()
-        print update.text
-    time.sleep(30)
-
-
+try:
+	mentions = api.GetMentions(since_id=last)
+except twitter.TwitterError:
+	print False
+	
+for m in mentions:
+    magnet = search(m.text.replace(ACCOUNT_NAME, ""))
+    update_str = "@%s %s" % (m.user.screen_name, shorten_magnet(magnet))
+try:
+	update = api.PostUpdate(update_str, in_reply_to_status_id=m.id)
+except twitter.TwitterError:
+	print False
+f = open("last.id", "w")
+f.write("%d\n" % (m.id,))
+f.close()
+print update.text
+time.sleep(30)
 
